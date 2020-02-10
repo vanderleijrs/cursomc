f0e04d7857583ffa642e02edc9fb73792a2252ce
vanderleijrs <vanderleijrs1977@gmail.com>
2020-02-10 16:49:57 -0300

    Banco de dados h2 e criação da base de dados

--- src/main/java/com/vanderlei/cursomc/domain/Categoria.java
+++ src/main/java/com/vanderlei/cursomc/domain/Categoria.java
@@ -2,9 +2,15 @@ package com.vanderlei.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
 public class Categoria implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)//Gerador de chave primária h2
 	private Integer id;
 	private String nome;
 	
