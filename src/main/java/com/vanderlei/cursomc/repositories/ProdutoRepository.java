d33d595edf0310ef47ae3db7ef81587d7e464fca
vanderleijrs <vanderleijrs1977@gmail.com>
2020-02-11 10:44:58 -0300

    Produto e associação muito para muitos

---
+++ src/main/java/com/vanderlei/cursomc/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.vanderlei.cursomc.repositories;
+
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.vanderlei.cursomc.domain.Categoria;
+import com.vanderlei.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
+	
+}
