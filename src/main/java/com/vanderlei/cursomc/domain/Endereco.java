3475b22270e2f6a4f1794a92d23762eccad3c408
vanderleijrs <vanderleijrs1977@gmail.com>
2020-02-13 12:40:14 -0300

    EndPoit/ clientes/{id} disponivel

--- src/main/java/com/vanderlei/cursomc/domain/Endereco.java
+++ src/main/java/com/vanderlei/cursomc/domain/Endereco.java
@@ -8,6 +8,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 @Entity
 public class Endereco implements Serializable{
 	private static final long serialVersionUID = 1L;
@@ -19,6 +21,7 @@ public class Endereco implements Serializable{
 	private String complemento;
 	private String bairro;
 	private String cep;
+	@JsonBackReference
 	@ManyToOne
 	@JoinColumn(name="cliente_id")
 	private Cliente cliente;
---
+++ src/main/java/com/vanderlei/cursomc/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.vanderlei.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import com.vanderlei.cursomc.domain.Cliente;
+import com.vanderlei.cursomc.services.ClienteService;
+
+import javassist.tools.rmi.ObjectNotFoundException;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	@Autowired//instancia automaticamente
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException{
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+		
+	}
+}
